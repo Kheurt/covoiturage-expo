8ddc92ef3e6cedf52317e804986f36e4f34ece31
Azoa <azoasophie13@gmail.com>
2022-12-14 08:59:32 +0100

    1er commit AZOA

--- components/mycars/mycars.jsx
+++ components/mycars/mycars.jsx
@@ -1,10 +1,20 @@
-class MyCars extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <div>MyCars Component</div>
-        );
-    }
+function MyCars() {
+    return ( 
+        <div>
+            <div></div> //div des icones d'en-tête
+            <div></div> //div du texte MY CARS
+            <div></div> //div de l'image du véhicule 
+            <div></div> // div de la liste des véhicules
+            <button></button> // 
+
+
+
+
+
+
+
+        </div> 
+    );
 }
- 
+
 export default MyCars;
