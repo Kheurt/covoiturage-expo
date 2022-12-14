49dc63fb1b827476cb3a350f81e343a7f130cde3
Kheurt <59851560+Kheurt@users.noreply.github.com>
2022-12-14 08:55:46 +0100

    Sum

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MyCars from './components/mycars/mycars';
 
 function HomeScreen() {
   return (
@@ -19,7 +20,10 @@ export default function App() {
   return ( 
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="MyCars" component={MyCars} />
       </Stack.Navigator>
     </NavigationContainer>
    );
